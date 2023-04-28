ad7768adf9bbd3b0394646698cfa6d8a5686198b
robert-juang <zdjiang20001225@gmail.com>
2023-04-28 02:23:50 -0400

    mroe changes to project structure

--- server/app.js
+++ server/app.js
@@ -16,7 +16,9 @@ const localStrategy = require( "passport-local");
 const app = express(); 
 
 const corsOptions ={
+  origin:'http://localhost:3000', 
   credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
 }
 
 const sessionOptions = {
